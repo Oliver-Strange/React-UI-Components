8cd31926614be3a7f5d30c42260aa7458acc73bf
Oliver-Strange <preston.burton@gmail.com>
2019-04-22 16:15:16 -0700

    forgot about commiting, everything but footer done, needs style polishing

---
+++ Project-I-Social-Card/social-card/src/components/FooterComponents/Footer.js
@@ -0,0 +1,11 @@
+// import React from 'react';
+// import './Footer.css';
+
+// const Footer = () => {
+//     return(
+//         <div className='footer'>
+//         <i class="fa fa-comment" aria-hidden="true"></i>
+
+//         </div>
+//     );
+// }
--- Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
+++ Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
@@ -1,2 +1,14 @@
 import React from 'react';
 import './Header.css';
+
+const HeaderTitle = () => {
+    return(
+        <div className='headerTitle'>
+        <h1>Lambda School</h1>
+        <h3 className="handle">@LambdaSchool</h3>
+        <h3 className="timeStamp">time</h3>
+        </div>
+    );
+}
+
+export default HeaderTitle;
