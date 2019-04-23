ce99d3584d8c6c95f680abf54cac7107f76b58a3
Oliver-Strange <preston.burton@gmail.com>
2019-04-23 13:30:05 -0700

    added basic layout to app, calc display, numberbutton, and actionbutton, imported and exported all of them, added a border to show calc area

--- Project-II-Calculator/calculator/src/components/ButtonComponents/ActionButton.js
+++ Project-II-Calculator/calculator/src/components/ButtonComponents/ActionButton.js
@@ -1,2 +1,13 @@
 import React from 'react';
 import './Button.css';
+
+const ActionButton = props => {
+    return(
+        <div className="actionButtons">
+            <h1 className="clear">clear</h1>
+            <h1 className="zero">0</h1>
+        </div>
+    );
+}
+
+export default ActionButton;
--- Project-II-Calculator/calculator/src/components/ButtonComponents/NumberButton.js
+++ Project-II-Calculator/calculator/src/components/ButtonComponents/NumberButton.js
@@ -1,2 +1,12 @@
 import React from 'react';
 import './Button.css';
+
+const NumButton = props => {
+    return(
+        <div className="numButton">
+            <button className={props.buttonStyle}>{props.text}</button>
+        </div>
+    );
+}
+
+export default NumButton;
